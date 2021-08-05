97c36eb5ceb219d460f05011988ceb7a3315c348
Nsadaa <sadaruwannilshan@gmail.com>
2021-08-05 14:35:26 +0530

    Add Form Validation

--- static/script.js
+++ static/script.js
@@ -7,3 +7,19 @@ function clear_function(){
 }
 
 function back_function(){ window.location="C:/Users/SADARUWAN/PycharmProjects/Loan_Status_Classifier/templates/index.html"; }
+
+
+function validation(){
+    var loan_amount = document.getElementById("loanamount");
+
+    if (loan_amount.value.trim() == "" )
+    {
+        alert("Loan Amount Is Empty")
+        return false;
+    }
+
+    else
+    {
+        true;
+    }
+}
