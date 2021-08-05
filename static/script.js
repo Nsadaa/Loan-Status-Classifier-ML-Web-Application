50bde60d9cb87f5eea0857910c30a931570dc608
Nsadaa <sadaruwannilshan@gmail.com>
2021-08-05 11:45:30 +0530

    Project Files

---
+++ static/script.js
@@ -0,0 +1,9 @@
+
+function view_function(){ window.location="C:/Users/SADARUWAN/PycharmProjects/Loan_Status_Classifier/templates/form.html"; }
+
+function clear_function(){
+
+    document.getElementById("form").reset();
+}
+
+function back_function(){ window.location="C:/Users/SADARUWAN/PycharmProjects/Loan_Status_Classifier/templates/index.html"; }
